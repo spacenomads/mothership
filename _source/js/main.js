ce7fd113012608b9141d3784fa1e2956a42a3d0e
Carlos Mañas <carlos@spacenomads.com>
2020-05-02 19:22:48 +0200

    Add new landing

--- _source/js/main.js
+++ _source/js/main.js
@@ -1,1 +1,28 @@
-console.log('🦄 Ready');
+const body = document.querySelector('body');
+
+
+
+
+
+function startAnimation(page, time) {
+	if ( page.classList.contains('ready') ) {
+		setTimeout(function() {
+			page.classList.add('has-asteroid');
+		}, time);
+	}
+}
+
+
+
+
+
+function initPage(page) {
+	page.classList.add('ready');
+	startAnimation(page, 1000);
+}
+
+
+
+
+
+initPage(body);
