d0da26d96d4a286463a21c0b3bb21eec0f7965a0
Carlos Mañas <carlos@spacenomads.com>
2024-06-29 16:37:48 +0200

    refactor: use the new vedeeo links and improve seasons block

--- _src/_data/seasons.js
+++ _src/_data/seasons.js
@@ -1,395 +1,544 @@
 module.exports = {
-	s3: [
-		{
-			name: 's03_e01 - Estilos para lista de tareas',
-			videos: [
-				{
-					link: 'https://youtu.be/vSTdG3JmebI',
-					duration: 'Casi hora y media',
-					time: 'PT1H23M'
-				}
-			]
+	s3emailResponsive: {
+		playList: {
+			name: 'Email responsive',
+			link: 'https://veedeo.org/w/p/o1auXiXyZhN3XwnN8D3R9M'
 		},
-		{
-			name: 's03_e02 - Generador de estáticos: 11ty (1/2)',
-			videos: [
-				{
-					link: 'https://youtu.be/kk6PEE6w8iA',
-					duration: 'Casi hora y media',
-					time: 'PT1H20M'
-				}
-			]
+		items: [
+			{
+				name: 's03_e05 - [serie] Introducción al email responsive /1',
+				videos: [
+					{
+						duration: 'Casi hora y media',
+						time: 'PT1H29M',
+						ytLink: 'https://youtu.be/qPSO_ixTh0E',
+						link: 'https://veedeo.org/w/tJuqXSEEGViF3ap6LNXYXB'
+					}
+				]
+			},
+			{
+				name: 's03_e06 - [serie] Mi primer email, ¡Chispas! /2',
+				videos: [
+					{
+						duration: 'Casi tres horas',
+						time: 'PT2H47M',
+						ytLink: 'https://youtu.be/44UbfyU6rlo',
+						link: 'https://veedeo.org/w/8fdzMeFY2xFY7nJydHEAED'
+					}
+				]
+			},
+			{
+				name: 's03_e07 - [serie] Email “The Eye” /3',
+				videos: [
+					{
+						duration: 'Casi dos horas',
+						time: 'PT1H58M',
+						ytLink: 'https://youtu.be/wycUkl9Yzvk',
+						link: 'https://veedeo.org/w/6D7gmaez9uTzb9Ghtt22Cy'
+					}
+				]
+			},
+			{
+				name: 's03_e08 - [serie] Semántica, módulos y email "The Eye" /4',
+				videos: [
+					{
+						duration: 'Tres horas',
+						time: 'PT3H01M',
+						ytLink: 'https://youtu.be/wB7uRu_IVZ0',
+						link: 'https://veedeo.org/w/quxPRrcCoWH5DHYY3XeQHw'
+					}
+				]
+			},
+			{
+				name: 's03_e09 - [serie] Email "real" /5',
+				videos: [
+					{
+						duration: 'Tres horas',
+						time: 'PT2H57M',
+						ytLink: 'https://youtu.be/YwrBHsmTinM',
+						link: 'https://veedeo.org/w/2Kzx1vSmYxANpjnC5FoCva'
+					}
+				]
+			},
+			{
+				name: 's03_e10 - [serie] Email "real" /6',
+				videos: [
+					{
+						duration: 'Tres horas',
+						time: 'PT3H06M',
+						ytLink: 'https://youtu.be/O8bV4rE_0Ck',
+						link: 'https://veedeo.org/w/nYAfTfgYnY5hfF42AxMCbk'
+					}
+				]
+			},
+			{
+				name: 's03_e11 - [serie] Email "real" /7',
+				videos: [
+					{
+						duration: 'Casi 4 horas',
+						time: 'PT3H50M',
+						ytLink: 'https://youtu.be/zMlxTZczRAE',
+						link: 'https://veedeo.org/w/udJJ5kYudGupMnKwMaFD31'
+					}
+				]
+			},
+			{
+				name: 's03_e12 - [serie] Email responsive: tips /8',
+				videos: [
+					{
+						duration: '2 horas',
+						time: 'PT2H08M',
+						ytLink: 'https://youtu.be/zCJg1h4y8jI',
+						link: 'https://veedeo.org/w/1us3Ww3v7yGx1ugRjfmW9U'
+					}
+				]
+			}
+		]
+	},
+	s3: {
+		playList: {
+			name: 'Temporada 3',
+			link: 'https://veedeo.org/w/p/sGNXQoPyVYbpHmyWgZok6S'
 		},
-		{
-			name: 's03_e03 - Generador de estáticos: 11ty (2/2)',
-			videos: [
-				{
-					link: 'https://youtu.be/9vXR57K3JQk',
-					duration: 'Más de hora y media',
-					time: 'PT1H38M'
-				}
-			]
+		items: [
+			{
+				name: 's03_e01 - Estilos para lista de tareas',
+				videos: [
+					{
+						duration: 'Casi hora y media',
+						time: 'PT1H23M',
+						ytLink: 'https://youtu.be/vSTdG3JmebI',
+						link: 'https://veedeo.org/w/eMqAw2tpMppMD1PwwwcKuz'
+					}
+				]
+			},
+			{
+				name: 's03_e02 - Generador de estáticos: 11ty (1/2)',
+				videos: [
+					{
+						duration: 'Casi hora y media',
+						time: 'PT1H20M',
+						ytLink: 'https://youtu.be/kk6PEE6w8iA',
+						link: 'https://veedeo.org/w/2QDS4AuRqr5VrJMwCmnFWk'
+					}
+				]
+			},
+			{
+				name: 's03_e03 - Generador de estáticos: 11ty (2/2)',
+				videos: [
+					{
+						duration: 'Más de hora y media',
+						time: 'PT1H38M',
+						ytLink: 'https://youtu.be/9vXR57K3JQk',
+						link: 'https://veedeo.org/w/bdDyuwWifHYmDf2hm4CMMJ'
+					}
+				]
+			},
+			{
+				name: 's03_e04 - Input de tipo range',
+				videos: [
+					{
+						duration: '2h',
+						time: 'PT1H58M',
+						ytLink: 'https://youtu.be/USKQ6zw9Jaw',
+						link: 'https://veedeo.org/w/3WEkJqG64cJVibMTrwuQuu'
+					}
+				]
+			},
+			{
+				name: 's03_e05 - [serie] Introducción al email responsive /1',
+				videos: [
+					{
+						duration: 'Casi hora y media',
+						time: 'PT1H29M',
+						ytLink: 'https://youtu.be/qPSO_ixTh0E',
+						link: 'https://veedeo.org/w/tJuqXSEEGViF3ap6LNXYXB'
+					}
+				]
+			},
+			{
+				name: 's03_e06 - [serie] Mi primer email, ¡Chispas! /2',
+				videos: [
+					{
+						duration: 'Casi tres horas',
+						time: 'PT2H47M',
+						ytLink: 'https://youtu.be/44UbfyU6rlo',
+						link: 'https://veedeo.org/w/8fdzMeFY2xFY7nJydHEAED'
+					}
+				]
+			},
+			{
+				name: 's03_e07 - [serie] Email “The Eye” /3',
+				videos: [
+					{
+						duration: 'Casi dos horas',
+						time: 'PT1H58M',
+						ytLink: 'https://youtu.be/wycUkl9Yzvk',
+						link: 'https://veedeo.org/w/6D7gmaez9uTzb9Ghtt22Cy'
+					}
+				]
+			},
+			{
+				name: 's03_e08 - [serie] Semántica, módulos y email "The Eye" /4',
+				videos: [
+					{
+						duration: 'Tres horas',
+						time: 'PT3H01M',
+						ytLink: 'https://youtu.be/wB7uRu_IVZ0',
+						link: 'https://veedeo.org/w/quxPRrcCoWH5DHYY3XeQHw'
+					}
+				]
+			},
+			{
+				name: 's03_e09 - [serie] Email "real" /5',
+				videos: [
+					{
+						duration: 'Tres horas',
+						time: 'PT2H57M',
+						ytLink: 'https://youtu.be/YwrBHsmTinM',
+						link: 'https://veedeo.org/w/2Kzx1vSmYxANpjnC5FoCva'
+					}
+				]
+			},
+			{
+				name: 's03_e10 - [serie] Email "real" /6',
+				videos: [
+					{
+						duration: 'Tres horas',
+						time: 'PT3H06M',
+						ytLink: 'https://youtu.be/O8bV4rE_0Ck',
+						link: 'https://veedeo.org/w/nYAfTfgYnY5hfF42AxMCbk'
+					}
+				]
+			},
+			{
+				name: 's03_e11 - [serie] Email "real" /7',
+				videos: [
+					{
+						duration: 'Casi 4 horas',
+						time: 'PT3H50M',
+						ytLink: 'https://youtu.be/zMlxTZczRAE',
+						link: 'https://veedeo.org/w/udJJ5kYudGupMnKwMaFD31'
+					}
+				]
+			},
+			{
+				name: 's03_e12 - [serie] Email responsive: tips /8',
+				videos: [
+					{
+						duration: '2 horas',
+						time: 'PT2H08M',
+						ytLink: 'https://youtu.be/zCJg1h4y8jI',
+						link: 'https://veedeo.org/w/1us3Ww3v7yGx1ugRjfmW9U'
+					}
+				]
+			}
+		]
+	},
+	s2: {
+		playList: {
+			name: 'Temporada 2',
+			link: 'https://veedeo.org/w/p/uCBJKnBFX6BTtTrsTtCm56'
 		},
-		{
-			name: 's03_e04 - Input de tipo range',
-			videos: [
-				{
-					link: 'https://youtu.be/USKQ6zw9Jaw',
-					duration: '2h',
-					time: 'PT1H58M'
-				}
-			]
+		items: [
+			{
+				name: 'S02E01 - Botones',
+				videos: [
+					{
+						duration: 'Casi 2h',
+						time: 'PT1H46M',
+						ytLink: 'https://youtu.be/EPJY2-N5yFw',
+						link: 'https://veedeo.org/w/rkznNPw7CqMCvkETXYCEnu'
+					}
+				]
+			},
+			{
+				name: 'S02E02 - Input:file',
+				videos: [
+					{
+						duration: '1h, ma o meno',
+						time: 'PT1H5M',
+						ytLink: 'https://youtu.be/1kN-p2IPsBs',
+						link: 'https://veedeo.org/w/7tycr1NpgBFAG1ps38sz7i'
+					}
+				]
+			},
+			{
+				name: 'S02E03 - Modales (1)',
+				videos: [
+					{
+						duration: 'Poco más de 1h y cuarto',
+						time: 'PT1H16M',
+						ytLink: 'https://youtu.be/j3P2a0A_XdI',
+						link: 'https://veedeo.org/w/pyKCn2EbvNik2KNr5HmoiE'
+					}
+				]
+			},
+			{
+				name: 'S02E04 - Modales (2)',
+				videos: [
+					{
+						duration: 'Casi 1h y cuarto',
+						time: 'PT1H11M',
+						ytLink: 'https://youtu.be/XtGzGPbaO_Y',
+						link: 'https://veedeo.org/w/xpPHNCE8jKqv5uNQyjbgKe'
+					}
+				]
+			},
+			{
+				name: 'S02E05 - Modales (3)',
+				videos: [
+					{
+						duration: 'Casi 1h y media',
+						time: 'PT1H25M',
+						ytLink: 'https://youtu.be/MOx-_22j9Pc',
+						link: 'https://veedeo.org/w/ggkyiieBL6GVPRKo4cXyGW'
+					}
+				]
+			},
+			{
+				name: 'S02E06 - Menú responsive (1)',
+				videos: [
+					{
+						duration: 'Casi 1h y cuarto',
+						time: 'PT1H12M',
+						ytLink: 'https://youtu.be/D4W_EL8W_1o',
+						link: 'https://veedeo.org/w/5KCXqcmqZ5SG4KQRurntcr'
+					}
+				]
+			},
+			{
+				name: 'S02E07 - Menú responsive (2)',
+				videos: [
+					{
+						duration: 'Casi 1h y cuarto',
+						time: 'PT1H13M',
+						ytLink: 'https://youtu.be/rpJOkF_bEA4',
+						link: 'https://veedeo.org/w/jv9xnrHBJY4NKFQfgvxVNg'
+					}
+				]
+			},
+			{
+				name: 'S02E08 - Menú responsive (3)',
+				videos: [
+					{
+						duration: '2h y media',
+						time: 'PT2H34M',
+						ytLink: 'https://youtu.be/Xsv9NlWP21w',
+						link: 'https://veedeo.org/w/vhXD76QDFtC946j12EmrVj'
+					}
+				]
+			},
+			{
+				name: 'S02E09 - CSS Comb',
+				videos: [
+					{
+						duration: 'Media hora larga',
+						time: 'PT38M',
+						ytLink: 'https://youtu.be/mDZu_E4pgok',
+						link: 'https://veedeo.org/w/jzkppeCdoEnR73JHweAgNX'
+					}
+				]
+			},
+			{
+				name: 'S02E10 - Checkbox y radio, accesibles y sin JS',
+				videos: [
+					{
+						duration: 'Casi 2h',
+						time: 'PT1H49M',
+						ytLink: 'https://youtu.be/wf0R8G5kY-Q',
+						link: 'https://veedeo.org/w/pHCneAVGrsMsx1HT5jg7su'
+					}
+				]
+			},
+			{
+				name: 'S02E11 - Figma para frontenders',
+				videos: [
+					{
+						duration: 'Casi 1h',
+						time: 'PT54M',
+						ytLink: 'https://youtu.be/-aA27u48II4',
+						link: 'https://veedeo.org/w/nwF3NnpiJ5U2RHuvhjJkpG'
+					}
+				]
+			},
+			{
+				name: 'S02E12 - BEM',
+				videos: [
+					{
+						duration: 'Casi 45min',
+						time: 'PT44M',
+						ytLink: 'https://youtu.be/tDF4qlFkfeA',
+						link: 'https://veedeo.org/w/2YDdUDjAMj3sdvN4F93Wjh'
+					}
+				]
+			}
+		]
+	},
+	s1: {
+		playList: {
+			name: 'Temporada 1',
+			link: 'https://veedeo.org/w/p/kTJ9PkTLJNPbs6LspMX4Mh'
 		},
-		{
-			name: 's03_e05 - [serie] Introducción al email responsive /1',
-			videos: [
-				{
-					link: 'https://youtu.be/qPSO_ixTh0E',
-					duration: 'Casi hora y media',
-					time: 'PT1H29M'
-				}
-			]
-		},
-		{
-			name: 's03_e06 - [serie] Mi primer email, ¡Chispas! /2',
-			videos: [
-				{
-					link: 'https://youtu.be/44UbfyU6rlo',
-					duration: 'Casi tres horas',
-					time: 'PT2H47M'
-				}
-			]
-		},
-		{
-			name: 's03_e07 - [serie] Email “The Eye” /3',
-			videos: [
-				{
-					link: 'https://youtu.be/wycUkl9Yzvk',
-					duration: 'Casi dos horas',
-					time: 'PT1H58M'
-				}
-			]
-		},
-		{
-			name: 's03_e08 - [serie] Semántica, módulos y email "The Eye" /4',
-			videos: [
-				{
-					link: 'https://youtu.be/wB7uRu_IVZ0',
-					duration: 'Tres horas',
-					time: 'PT3H01M'
-				}
-			]
-		},
-		{
-			name: 's03_e09 - [serie] Email "real" /5',
-			videos: [
-				{
-					link: 'https://youtu.be/YwrBHsmTinM',
-					duration: 'Tres horas',
-					time: 'PT2H57M'
-				}
-			]
-		},
-		{
-			name: 's03_e10 - [serie] Email "real" /6',
-			videos: [
-				{
-					link: 'https://youtu.be/O8bV4rE_0Ck',
-					duration: 'Tres horas',
-					time: 'PT3H06M'
-				}
-			]
-		},
-		{
-			name: 's03_e11 - [serie] Email "real" /7',
-			videos: [
-				{
-					link: 'https://youtu.be/zMlxTZczRAE',
-					duration: 'Casi 4 horas',
-					time: 'PT3H50M'
-				}
-			]
-		},
-		{
-			name: 's03_e12 - [serie] Email responsive: tips /8',
-			videos: [
-				{
-					link: 'https://youtu.be/zCJg1h4y8jI',
-					duration: '2 horas',
-					time: 'PT2H08M'
-				}
-			]
+		items: [
+			{
+				name: 'S01E00 - Tooltip',
+				videos: [
+					{
+						duration: '45 min',
+						time: 'PT45M',
+						ytLink: 'https://www.youtube.com/watch?v=pwtNFzrbNAM',
+						link: 'https://veedeo.org/w/asiQPbQ99Seqh7Ek6fAeFA'
+					},
+					{
+						duration: 'Casi 1h',
+						time: 'PT1H',
+						ytLink: 'https://www.youtube.com/watch?v=SqGFoHnJg60',
+						link: 'https://veedeo.org/w/3GQ8bXaUH85MVGWPs7YLHR'
+					}
+				]
+			},
+			{
+				name: 'S01E01 - HTML (y semántica)',
+				videos: [
+					{
+						duration: '1h y cuarto',
+						time: 'PT1H15M',
+						ytLink: 'https://www.youtube.com/watch?v=5vATBkG4Ijw',
+						link: 'https://veedeo.org/w/fM1BuiJHJgs9ZqhUDiAd5w'
+					}
+				]
+			},
+			{
+				name: 'S01E02 - Módulo de vídeos destacados',
+				videos: [
+					{
+						duration: 'Casi 1h',
+						time: 'PT1H',
+						ytLink: 'https://youtu.be/UTLDi4RBx0U',
+						link: 'https://veedeo.org/w/3MhA8EgSWkVGQ27KM6Lime'
+					}
+				]
+			},
+			{
+				name: 'S01E03 - Footer 1/2',
+				videos: [
+					{
+						duration: '1h y cuarto',
+						time: 'PT1H15M',
+						ytLink: 'https://youtu.be/5WGKZnxy4b4',
+						link: 'https://veedeo.org/w/1uADN7T6o16eqrL5k8CdTX'
+					}
+				]
+			},
+			{
+				name: 'S01E04 - Footer 2/2',
+				videos: [
+					{
+						duration: 'Dos horacas',
+						time: 'PT2H',
+						ytLink: 'https://youtu.be/tVqJOHIjB0w',
+						link: 'https://veedeo.org/w/wKmRZiADyBoBDpp9u3rXHV'
+					}
+				]
+			},
+			{
+				name: 'S01E05 - CSS, introducción y cómo se usa',
+				videos: [
+					{
+						duration: '1h y 20',
+						time: 'PT1H20M',
+						ytLink: 'https://youtu.be/nb1PVduHPME',
+						link: 'https://veedeo.org/w/33vqPM3pFCS6U51mEAqAvP'
+					}
+				]
+			},
+			{
+				name: 'S01E06 - CSS: Display',
+				videos: [
+					{
+						duration: 'Casi 1h',
+						time: 'PT48M',
+						ytLink: 'https://youtu.be/ccluG1Mwepg',
+						link: 'https://veedeo.org/w/1HBXyrER3ooxuJ9pNG7fJD'
+					}
+				]
+			},
+			{
+				name: 'S01E07 - CSS: Position',
+				videos: [
+					{
+						duration: 'Casi 2h',
+						time: 'PT1H45M',
+						ytLink: 'https://youtu.be/zXHPgzCWsx4',
+						link: 'https://veedeo.org/w/sP8wefJXetC1rwGVUEgW7P'
+					}
+				]
+			},
+			{
+				name: 'S01E08 - CSS: Flex',
+				videos: [
+					{
+						duration: 'Casi 3h',
+						time: 'PT2H53M',
+						ytLink: 'https://youtu.be/4p_rxHkF-sM',
+						link: 'https://veedeo.org/w/pwQVNr5mvWRCTohohyYyRs'
+					}
+				]
+			},
+			{
+				name: 'S01E09 - CSS: Grid',
+				videos: [
+					{
+						duration: 'Más de 3h',
+						time: 'PT3H21M',
+						ytLink: 'https://youtu.be/17bhRbGr_oo',
+						link: 'https://veedeo.org/w/oRSW2Y5gmRZ5Gi7PmqsFYt'
+					}
+				]
+			},
+			{
+				name: 'S01E10 - SASS/SCSS',
+				videos: [
+					{
+						duration: 'Dos horas y media',
+						time: 'PT2H32M',
+						ytLink: 'https://youtu.be/HSC4_yrzP94',
+						link: 'https://veedeo.org/w/ocSNeVMMezZ8w8PWNzb9j5'
+					}
+				]
+			},
+			{
+				name: 'S01E11 - VS Code',
+				videos: [
+					{
+						duration: 'Más de 1h y media',
+						time: 'PT1H39M',
+						ytLink: 'https://youtu.be/dLceE1kW604',
+						link: 'https://veedeo.org/w/1932PkF6mfQaMt9ibhdzqm'
+					}
+				]
+			},
+			{
+				name: 'S01E12 - Proyectos',
+				videos: [
+					{
+						duration: '1h y algo',
+						time: 'PT1H5M',
+						ytLink: 'https://youtu.be/_z5Ry7uY6NA',
+						link: 'https://veedeo.org/w/uxMhiSQd6mza6Bt2hK2Hy3'
+					}
+				]
+			}
+		]
+	},
+	channel: {
+		link: 'https://veedeo.org/c/maquetando_con_chuck/',
+		name: 'Maquetando con Chuck!',
+		playlists: 'https://veedeo.org/c/maquetando_con_chuck/video-playlists',
+		platform: {
+			name: 'Veedeo',
+			link: 'https://veedeo.org'
 		}
-	],
-	s2: [
-		{
-			name: 'S02E01 - Botones',
-			videos: [
-				{
-					link: 'https://youtu.be/EPJY2-N5yFw',
-					duration: 'Casi 2h',
-
-					time: 'PT1H46M'
-				}
-			]
-		},
-		{
-			name: 'S02E02 - Input:file',
-			videos: [
-				{
-					link: 'https://youtu.be/1kN-p2IPsBs',
-					duration: '1h, ma o meno',
-
-					time: 'PT1H5M'
-				}
-			]
-		},
-		{
-			name: 'S02E03 - Modales (1)',
-			videos: [
-				{
-					link: 'https://youtu.be/j3P2a0A_XdI',
-					duration: 'Poco más de 1h y cuarto',
-
-					time: 'PT1H16M'
-				}
-			]
-		},
-		{
-			name: 'S02E04 - Modales (2)',
-			videos: [
-				{
-					link: 'https://youtu.be/XtGzGPbaO_Y',
-					duration: 'Casi 1h y cuarto',
-
-					time: 'PT1H11M'
-				}
-			]
-		},
-		{
-			name: 'S02E05 - Modales (3)',
-			videos: [
-				{
-					link: 'https://youtu.be/MOx-_22j9Pc',
-					duration: 'Casi 1h y media',
-
-					time: 'PT1H25M'
-				}
-			]
-		},
-		{
-			name: 'S02E06- Menú responsive (1)',
-			videos: [
-				{
-					link: 'https://youtu.be/D4W_EL8W_1o',
-					duration: 'Casi 1h y cuarto',
-
-					time: 'PT1H12M'
-				}
-			]
-		},
-		{
-			name: 'S02E07- Menú responsive (2)',
-			videos: [
-				{
-					link: 'https://youtu.be/rpJOkF_bEA4',
-					duration: 'Casi 1h y cuarto',
-
-					time: 'PT1H13M'
-				}
-			]
-		},
-		{
-			name: 'S02E08- Menú responsive (3)',
-			videos: [
-				{
-					link: 'https://youtu.be/Xsv9NlWP21w',
-					duration: '2h y media',
-
-					time: 'PT2H34M'
-				}
-			]
-		},
-		{
-			name: 'S02E09 - CSS Comb',
-			videos: [
-				{
-					link: 'https://youtu.be/mDZu_E4pgok',
-					duration: 'Media hora larga',
-
-					time: 'PT38M'
-				}
-			]
-		},
-		{
-			name: 'S02E10 - Checkbox y radio, accesibles y sin JS',
-			videos: [
-				{
-					link: 'https://youtu.be/wf0R8G5kY-Q',
-					duration: 'Casi 2h',
-
-					time: 'PT1H49M'
-				}
-			]
-		},
-		{
-			name: 'S02E11 - Figma para frontenders',
-			videos: [
-				{
-					link: 'https://youtu.be/-aA27u48II4',
-					duration: 'Casi 1h',
-
-					time: 'PT54M'
-				}
-			]
-		},
-		{
-			name: 'S02E12 - BEM',
-			videos: [
-				{
-					link: 'https://youtu.be/tDF4qlFkfeA',
-					duration: 'Casi 45min',
-
-					time: 'PT44M'
-				}
-			]
-		}
-	],
-	s1:[
-		{
-			name: 'S01E00 - Tooltip',
-			videos: [
-				{
-					link: 'https://www.youtube.com/watch?v=pwtNFzrbNAM',
-					duration: '45 min',
-					time: 'PT45M'
-				},
-				{
-					link: 'https://www.youtube.com/watch?v=SqGFoHnJg60',
-					duration: 'Casi 1h',
-					time: 'PT1H'
-				}
-			]
-		},
-		{
-			name: 'S01E01 - HTML (y semántica)',
-			videos: [
-				{
-					link: 'https://www.youtube.com/watch?v=5vATBkG4Ijw',
-					duration: '1h y cuarto',
-					time: 'PT1H15M'
-				}
-			]
-		},
-		{
-			name: 'S01E02 - Módulo de vídeos destacados',
-			videos: [
-				{
-					link: 'https://youtu.be/UTLDi4RBx0U',
-					duration: 'Casi 1h',
-					time: 'PT1H'
-				}
-			]
-		},
-		{
-			name: 'S01E03 - Footer 1/2',
-			videos: [
-				{
-					link: 'https://youtu.be/5WGKZnxy4b4',
-					duration: '1h y cuarto',
-					time: 'PT1H15M'
-				}
-			]
-		},
-		{
-			name: 'S01E04 - Footer 2/2',
-			videos: [
-				{
-					link: 'https://youtu.be/tVqJOHIjB0w',
-					duration: 'Dos horacas',
-					time: 'PT2H'
-				}
-			]
-		},
-		{
-			name: 'S01E05 - CSS, introducción y cómo se usa',
-			videos: [
-				{
-					link: 'https://youtu.be/nb1PVduHPME',
-					duration: '1h y 20',
-					time: 'PT1H20M'
-				}
-			]
-		},
-		{
-			name: 'S01E06 - CSS: Display',
-			videos: [
-				{
-					link: 'https://youtu.be/ccluG1Mwepg',
-					duration: 'Casi 1h',
-					time: 'PT48M'
-				}
-			]
-		},
-		{
-			name: 'S01E07 - CSS: Position',
-			videos: [
-				{
-					link: 'https://youtu.be/zXHPgzCWsx4',
-					duration: 'Casi 2h',
-					time: 'PT1H45M'
-				}
-			]
-		},
-		{
-			name: 'S01E08 - CSS: Flex',
-			videos: [
-				{
-					link: 'https://youtu.be/4p_rxHkF-sM',
-					duration: 'Casi 3h',
-					time: 'PT2H53M'
-				}
-			]
-		},
-		{
-			name: 'S01E09 - CSS: Grid',
-			videos: [
-				{
-					link: 'https://youtu.be/17bhRbGr_oo',
-					duration: 'Más de 3h',
-					time: 'PT3H21M'
-				}
-			]
-		},
-		{
-			name: 'S01E10 - SASS/SCSS',
-			videos: [
-				{
-					link: 'https://youtu.be/HSC4_yrzP94',
-					duration: 'Dos horas y media',
-					time: 'PT2H32M'
-				}
-			]
-		},
-		{
-			name: 'S01E11 - VS Code',
-			videos: [
-				{
-					link: 'https://youtu.be/dLceE1kW604',
-					duration: 'Más de 1h y media',
-					time: 'PT1H39M'
-				}
-			]
-		},
-		{
-			name: 'S01E12 - Proyectos',
-			videos: [
-				{
-					link: 'https://youtu.be/_z5Ry7uY6NA',
-					duration: '1h y algo',
-					time: 'PT1H5M'
-				}
-			]
-		}
-	]
+	}
 };
